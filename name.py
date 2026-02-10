1961eb4f26c942049ad30c41f6d14a55fdd8a9f7
jchandd <jaydenc243@deltalearns.ca>
2026-02-10 10:35:34 -0800

    Added all pseudo code

---
+++ name.py
@@ -0,0 +1,33 @@
+# Import random module to generate number
+
+# Function to get valid integer input while handling error
+
+# Function to get a valid 'y' or 'n' response
+
+# Function to play a round of the game
+
+# Ask number range
+
+# Ensure large number is greater than small number
+
+# Ask for amount of attempts
+
+# Generate random number
+
+# Track amount of attempts
+
+# Loop for user guesses
+
+# Check if guess it too low or high
+
+# Display success message if geussed correctly
+
+# If max attempts are used, reveal correct number
+
+# Main game loop
+
+# Ask for user name and greet them
+
+# Ask if they want to play again, and have to press either 'y' or 'n'
+
+# Run game
