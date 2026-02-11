194a30350d0ddb75d5181a7a55f56ed9c6277897
jchandd <jaydenc243@deltalearns.ca>
2026-02-11 09:46:18 -0800

    added parts of code for number guesser

--- name.py
+++ name.py
@@ -1,15 +1,35 @@
 # Import random module to generate number
-
+import random
 
 # Function to get valid integer input while handling error
 
 # Function to get a valid 'y' or 'n' response
 
+
 # Function to play a round of the game
+def get_valid_integer(prompt):
+    while True:
+        try:
+            value = int(input(prompt).strip())  # .strip() removes extra spaces
+            return value
+        except ValueError:
+            print(" Invalid input. Please enter a valid whole number.")
+
 
 # Ask number range
 
+
 # Ensure large number is greater than small number
+def get_number_range():
+    while True:
+        low = get_valid_integer("Enter the Low number of the range: ")
+        high = get_valid_integer("Enter the High number of the range: ")
+
+        if high > low:
+            return low, high
+        else:
+            print(" High number must be larger than low number. Please try again.")
+
 
 # Ask for amount of attempts
 
